c209648dcd1e0168477452d7d6999ff0cc55c82d
SRIPARNA ROY <sriparna.roy3640@gmail.com>
2022-12-01 17:47:40 +0530

    tcommit

---
+++ ch11/index.js
@@ -0,0 +1,47 @@
+// import EventEmitter from 'events';
+
+// // creating class
+// class MyEmitter extends EventEmitter{}
+
+// // creating object
+// const myEmitter = new MyEmitter();
+
+
+// // example 1
+// // register event listener
+// myEmitter.on('event', ()=>{
+//     console.log('event occured');
+// })
+// // triggers the event
+// myEmitter.emit('event');
+
+
+// // example2
+// // register event listener
+// let m = 0;
+// myEmitter.on('event', ()=>{
+//     console.log(++m);
+// })
+// // triggers the event
+// myEmitter.emit('event');
+
+// // example3
+// let m = 0;
+// myEmitter.once('event', ()=>{
+//     console.log(++m);
+// })
+// // triggers the event
+// myEmitter.emit('event');
+
+// // example 4
+// let m = 0;
+// myEmitter.once('event', ()=>{
+//     console.log(++m);
+// })
+// // triggers the event
+// myEmitter.emit('event');
+// myEmitter.emit('event');
+
+
+
+ 
