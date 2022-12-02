e38b156977ed7b11f6dc486724644d4322b524c9
SRIPARNA ROY <sriparna.roy3640@gmail.com>
2022-12-02 08:44:37 +0530

    se2commit

---
+++ ch13/index.js
@@ -0,0 +1,23 @@
+// create web server- http Module
+import http from 'http';
+
+// const server = http.createServer((req, res)=>{
+//     res.setHeader('Content- Type', 'text/plain');
+// res.end('Response from server');
+// });
+// server.listen(8000, 'localhost', ()=>{
+//     console.log('server running at http://localhost:8000');
+// });
+
+
+const server = http.createServer((req, res)=>{
+res.setHeader('Content- Type', 'text/plain');
+res.end('Response from server');
+});
+
+const PORT = process.env.PORT || 8000;
+const HOST = 'localhost';
+
+server.listen(PORT, HOST , ()=>{
+    console.log('server running at http://localhost:8000');
+});
